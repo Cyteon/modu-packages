028e4138b1524384bb1d6775050618a3cdcef7f0
cyteon <cyteon@hackclub.app>
2024-12-28 16:38:40 +0100

    navbar

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,10 @@
+<nav class="flex flex-col md:flex-row px-1 md:px-16 py-2">
+    <h1 class="text-3xl font-bold leading-relaxed bg-clip-text text-transparent bg-gradient-to-r from-ctp-red to-75% to-ctp-yellow">Modu Packages</h1>
+
+    <div class="ml-auto flex my-auto w-full max-w-[700px] bg-ctp-crust rounded-md border border-ctp-surface0 mb-2 lg:mb-0 text-lg">
+        <input type="text" class="leading-relaxed" placeholder="Search for packages" />
+        <button class="rounded-tr-md rounded-br-md bg-ctp-mantle font-semibold px-2">Search</button>
+    </div>
+
+    <a href="/login" class="ml-auto text-2xl h-fit my-auto leading-relaxed px-2 a-btn rounded-md">Login</a>
+</nav>
