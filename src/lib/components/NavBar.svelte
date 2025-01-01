acd416ffaf127e317dfdf4cc3c5d9253a9d2c78e
cyteon <cyteon@hackclub.app>
2025-01-01 11:04:12 +0100

    modu (packages)

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -3,6 +3,7 @@
     import state from "$lib/state.svelte";
     import { removeCookie } from "typescript-cookie";
     import { browser } from "$app/environment";
+    import { goto } from "$app/navigation";
 
     let showUserMenu = false;
     let query = "";
@@ -13,14 +14,14 @@
 </script>
 
 <nav class="flex flex-col md:flex-row px-1 md:px-16 py-2">
-    <a href="/" class="text-3xl font-bold leading-relaxed bg-clip-text text-transparent bg-gradient-to-r from-ctp-red to-75% to-ctp-yellow">Modu Packages</a>
+    <a href="/" class="text-3xl font-bold leading-relaxed bg-clip-text text-transparent bg-gradient-to-r from-ctp-red to-75% to-ctp-yellow">Modu (Packages)</a>
 
     <div class="ml-auto flex my-auto w-full max-w-[700px] bg-ctp-crust rounded-md border border-ctp-surface0 mb-2 lg:mb-0 text-lg">
         <input type="text" class="leading-relaxed" placeholder="Search for packages" bind:value={query} />
         <button class="rounded-tr-md rounded-br-md bg-ctp-mantle font-semibold px-2" on:click={() => {
             if (query.trim() === "") return;
 
-            window.location.href = `/search?q=${query}`;
+            goto(`/search?q=${query}`);
         }}>Search</button>
     </div>
 
